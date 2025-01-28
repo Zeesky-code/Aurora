e300ed9eb7e4668d222ae6c07b8e39577f61246f
Zainab Lawal <zainablaw1012@gmail.com>
2025-01-28 13:36:51 +0300

    Fix: cleanup TaskStatusChange

--- src/main/java/com/aurora/core/model/TaskStatusChange.java
+++ src/main/java/com/aurora/core/model/TaskStatusChange.java
@@ -3,13 +3,7 @@ package com.aurora.core.model;
 import java.time.Instant;
 
 public class TaskStatusChange {
-    private TaskStatus status;
-    private Instant time;
-    private String worker;
 
     public TaskStatusChange(TaskStatus newStatus, Instant now, String assignedWorker) {
-        this.status = newStatus;
-        this.time = now;
-        this.worker = assignedWorker;
     }
 }
