6f95922511431cb8f314e4b3a424cefe4a617599
Zainab Lawal <zainablaw1012@gmail.com>
2025-01-28 15:01:00 +0300

    Feat: Register and Stop worker

--- src/main/java/com/aurora/worker/Worker.java
+++ src/main/java/com/aurora/worker/Worker.java
@@ -4,8 +4,12 @@ import com.aurora.core.config.AuroraConfig;
 import com.aurora.core.exception.AuroraException;
 import com.aurora.core.model.*;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.leader.LeaderLatch;
 import java.util.concurrent.*;
+
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,7 +26,10 @@ public class Worker {
 
     public Worker(String workerId, String zkConnectString) {
         this.workerId = workerId;
-        this.curator = createCuratorFramework(zkConnectString);
+        this.curator = CuratorFrameworkFactory.builder()
+                .connectString(zkConnectString)
+                .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+                .build();
         this.leaderLatch = new LeaderLatch(curator, "/aurora/leader");
         this.taskExecutor = Executors.newFixedThreadPool(
                 AuroraConfig.getWorkerThreads()
@@ -32,6 +39,18 @@ public class Worker {
                 (t1, t2) -> t2.getPriority().getValue() - t1.getPriority().getValue()
         );
         this.taskProcessor = new TaskProcessor(curator, taskQueue);
+    }
+
+    private void registerWorker() {
+        try {
+            String workerPath = "/scheduler/workers/" + workerId;
+            curator.create()
+                    .creatingParentsIfNeeded()
+                    .withMode(CreateMode.EPHEMERAL)
+                    .forPath(workerPath);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to register worker", e);
+        }
     }
 
     public void start() {
@@ -45,6 +64,20 @@ public class Worker {
             throw new AuroraException("Failed to start worker", e);
         }
     }
+    public void stop() {
+        isRunning = false;
+        taskExecutor.shutdown();
+        try {
+            if (!taskExecutor.awaitTermination(60, TimeUnit.SECONDS)) {
+                taskExecutor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            taskExecutor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+        logger.info("Worker stopped: {}", workerId);
+    }
+
 
     private void startTaskProcessing() {
         isRunning = true;
